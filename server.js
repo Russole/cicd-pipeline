6963159255c6b377f0561e6e962bc1fed734f820
Russole <850905junior@gmail.com>
2024-10-31 14:35:20 +0800

    Change Server.js Port(80)

--- server.js
+++ server.js
@@ -11,6 +11,6 @@ app.get('/', (req, res) => {
     })
 })
 
-app.listen(8080, () => {
-    console.log('server running on port 8080')
+app.listen(80, () => {
+    console.log('server running on port 80')
 })
